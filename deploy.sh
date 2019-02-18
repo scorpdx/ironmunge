b3d0694396cc54144cdc6391664dae6ddb8cc999
J. Zebedee <zebedee@code.gripe>
2019-02-18 12:43:32 -0600

    Remove step to download warp-packer

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,6 @@ curl -Lo mingit32.zip https://github.com/git-for-windows/git/releases/download/v
 curl -Lo mingit64.zip https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/MinGit-2.20.1-busybox-64-bit.zip
 (echo -n "9817ab455d9cbd0b09d8664b4afbe4bbf78d18b556b3541d09238501a749486c *mingit64.zip" | sha256sum -c -) || exit 1
 
-# download and verify warp-packer
-curl -Lo warp-packer https://github.com/dgiagio/warp/releases/download/v0.3.0/linux-x64.warp-packer
-(echo -n "ba5da4d224077fffea78c7872df47413234e4ee179c941c821aae0b33bd9f594 *warp-packer" | sha256sum -c -) || exit 1
-
 # unzip MinGit 64 and add to pub-win-x64
 (unzip mingit64.zip -d pub-win-x64/Resources/git) || exit 1
 
