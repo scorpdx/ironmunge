d0d6289a90724f9a31caff2e533d398cb6c70a24
J. Zebedee <zebedee@code.gripe>
2019-02-21 23:24:32 -0600

    Set icons through rcdedit

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,19 @@ curl -Lo mingit64.zip https://github.com/git-for-windows/git/releases/download/v
 (echo -n "9817ab455d9cbd0b09d8664b4afbe4bbf78d18b556b3541d09238501a749486c *mingit64.zip" | sha256sum -c -) || exit 1
 
 # unzip MinGit 64 and add to pub-win-x64
-(unzip mingit64.zip -d pub-win-x64/Resources/git) || exit 1
+(unzip -o mingit64.zip -d pub-win-x64/Resources/git) || exit 1
 
 # unzip MinGit 32 and add to pub-win-x86
-(unzip mingit32.zip -d pub-win-x86/Resources/git) || exit 1
+(unzip -o mingit32.zip -d pub-win-x86/Resources/git) || exit 1
+
+# download and verify rcedit (assume x64 host)
+curl -Lo rcedit.exe https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe
+(echo -n "02e8e8c5d430d8b768980f517b62d7792d690982b9ba0f7e04163cbc1a6e7915 *rcedit.exe" | sha256sum -c -) || exit 1
+
+# set ironmunge icon
+rcedit pub-win-x64/ironmunge/ironmunge.exe --set-icon "assets/ironmunge.ico"
+rcedit pub-win-x86/ironmunge/ironmunge.exe --set-icon "assets/ironmunge.ico"
+
+# set SaveManager icon
+rcedit pub-win-x64/SaveManager/SaveManager.exe --set-icon "assets/savemanager.ico"
+rcedit pub-win-x86/SaveManager/SaveManager.exe --set-icon "assets/savemanager.ico"
