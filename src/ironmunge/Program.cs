72cf6fe4aba30ee330a65257588c13d74d26ea34
J. Zebedee <zebedee@code.gripe>
2019-04-09 18:51:36 -0500

    Add remote command-line option

--- src/ironmunge/Program.cs
+++ src/ironmunge/Program.cs
@@ -8,6 +8,9 @@ namespace ironmunge
     {
         [Option('n', "notifications", HelpText = "Play notification sounds while saving history", Default = true)]
         public bool Notifications { get; set; }
+
+        [Option('r', "remote", HelpText = "Remote server to push updates")]
+        public string Remote { get; set; }
     }
 
     class Program
