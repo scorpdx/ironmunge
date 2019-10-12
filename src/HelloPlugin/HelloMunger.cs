a61c9049258bea6ea12cc86770a79d99a2184863
J. Zebedee <zebedee@code.gripe>
2019-10-12 01:08:11 -0500

    Add example plugin

---
+++ src/HelloPlugin/HelloMunger.cs
@@ -0,0 +1,17 @@
+﻿using ironmunge.Plugins;
+using System;
+
+namespace HelloPlugin
+{
+    public class HelloMunger : IMunger
+    {
+        public string Name { get => "hello"; }
+        public string Description { get => "Displays hello message."; }
+
+        public int Execute()
+        {
+            Console.WriteLine("Hello !!!");
+            return 0;
+        }
+    }
+}
--- src/ironmunge/Program.cs
+++ src/ironmunge/Program.cs
@@ -1,6 +1,11 @@
 ﻿using CommandLine;
 using ironmunge.Common;
+using ironmunge.Plugins;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace ironmunge
 {
@@ -34,6 +39,8 @@ namespace ironmunge
                         return;
                     }
 
+                    LoadPlugins();
+
                     using (var cm = new ChangeMonitoring(o.GitLocation ?? GitHelpers.DefaultGitPath,
                                                          o.SaveGameLocation ?? DefaultSaveDir,
                                                          o.SaveHistoryLocation ?? DefaultSaveDir,
@@ -46,8 +53,8 @@ namespace ironmunge
                         ConsoleKeyInfo key;
                         while ((key = Console.ReadKey()).Key != ConsoleKey.Escape)
                         {
-                                //wait for key to exit
-                            }
+                            //wait for key to exit
+                        }
                     }
                 })
                 .WithNotParsed(o =>
@@ -61,6 +68,68 @@ namespace ironmunge
                     Console.WriteLine("Press ENTER to exit.");
                     Console.ReadLine();
                 });
+        }
+
+        static void LoadPlugins()
+        {
+            string[] pluginPaths = new string[]
+            {
+                @"..\src\HelloPlugin\bin\Debug\netcoreapp3.0\HelloPlugin.dll",
+                // Paths to plugins to load.
+            };
+
+            var mungers = pluginPaths
+                .Select(LoadPlugin)
+                .SelectMany(CreateCommands);
+
+            foreach (var munger in mungers)
+            {
+                Console.WriteLine($"{munger.Name}\t - {munger.Description}");
+            }
+
+
+        }
+
+        static Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading commands from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+
+        static IEnumerable<IMunger> CreateCommands(Assembly assembly)
+        {
+            int count = 0;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(IMunger).IsAssignableFrom(type))
+                {
+                    IMunger result = Activator.CreateInstance(type) as IMunger;
+                    if (result != null)
+                    {
+                        count++;
+                        yield return result;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}");
+            }
         }
     }
 }
