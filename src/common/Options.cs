0666a49e4547cb37caf5c8cf85418a86dbbb2391
J. Zebedee <zebedee@code.gripe>
2020-09-10 13:34:10 -0500

    Handle nullability in Options

--- src/common/Options.cs
+++ src/common/Options.cs
@@ -6,10 +6,10 @@ namespace Ironmunge.Common
 {
     public class Options
     {
-        public static string DefaultGitPath
+        public static string? DefaultGitPath
            => Directory.EnumerateFiles("./Resources/git/cmd/", "git*", SearchOption.TopDirectoryOnly).SingleOrDefault();
 
-        public static string DefaultJsonConverterPath
+        public static string? DefaultJsonConverterPath
             => Directory.EnumerateFiles("./Resources/", "ck2json*", SearchOption.TopDirectoryOnly).SingleOrDefault();
 
         [Option('s', "saveGames", HelpText = "Path of the Crusader Kings II save game directory")]
