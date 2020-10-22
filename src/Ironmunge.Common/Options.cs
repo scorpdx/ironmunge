e2fa62a43adb2a30a9ae26fd93c701595cde8c7f
J. Zebedee <zebedee@code.gripe>
2020-10-21 19:20:35 -0500

    Update folder structure

--- src/Ironmunge.Common/Options.cs
+++ src/Ironmunge.Common/Options.cs
@@ -1,5 +1,4 @@
 ﻿using CommandLine;
-using System;
 using System.IO;
 using System.Linq;
 
@@ -7,20 +6,19 @@ namespace Ironmunge.Common
 {
     public class Options
     {
-        public static string DefaultGitPath
-           => Directory.EnumerateFiles("./Resources/git/cmd/", "git*", SearchOption.TopDirectoryOnly).SingleOrDefault()
-            ?? throw new InvalidOperationException("Git was not found");
+        public static string? DefaultGitPath
+           => Directory.EnumerateFiles("./Resources/git/cmd/", "git*", SearchOption.TopDirectoryOnly).SingleOrDefault();
 
-        [Option('g', "game", HelpText = "Name of the game to monitor", Required = true)]
-        public string Game { get; set; }
+        public static string? DefaultJsonConverterPath
+            => Directory.EnumerateFiles("./Resources/", "ck2json*", SearchOption.TopDirectoryOnly).SingleOrDefault();
 
-        [Option('s', "save-games", HelpText = "Path of the save game directory", Required = true)]
-        public string SaveGameLocation { get; set; }
+        [Option('s', "saveGames", HelpText = "Path of the Crusader Kings II save game directory")]
+        public string? SaveGameLocation { get; set; }
 
-        [Option('h', "save-histories", HelpText = "Path of the ironmunge save history directory")]
+        [Option('h', "saveHistories", HelpText = "Path of the ironmunge save history directory")]
         public string? SaveHistoryLocation { get; set; }
 
-        [Option("git", HelpText = "Path of the git executable")]
+        [Option('g', "git", HelpText = "Path of the git executable")]
         public string? GitLocation { get; set; }
     }
 
