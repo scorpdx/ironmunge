4d88b19d3d658996176cd849d6f86fef82f7f384
J. Zebedee <zebedee@code.gripe>
2021-08-09 05:03:10 +0000

    Implement ironmunge as .NET hosted service (#22)
    * Update folder structure
    
    * Add Rakaly-based CK3 binary parsing
    
    * Update for handling ck3 bin files
    
    * Replace rakaly and CK3Bin with ck3json implementation
    
    * Add CK3 restores to SaveManager
    
    * Don't store json in repo
    
    * Update launch settings
    
    * Update packages and use new NAudio
    
    * Update ChronicleMunger
    
    * Break out IGame stub
    
    * Break out PluginManager
    
    * Rearrange with all games stub
    
    * Get CK3 plugin working
    
    * Make AddSaveAsync save again
    
    * Remove ILogger parameter from IGame
    
    * Add hosting dependency
    
    * Break out IronmungeOptions
    
    * Replace PluginManager with PluginProvider
    
    * Use ILogger in SaveMonitoring and log instead of console writing
    
    * Update Game.AddSaveAsync signature in SaveHistory
    
    * Add IronmungeWorker hosted background service
    
    * Update Program to host service

---
+++ src/ironmunge/IronmungeOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace ironmunge
+{
+    public class IronmungeOptions : Ironmunge.Common.Options
+    {
+        [Option('n', "notifications", HelpText = "Play notification sounds while saving history", Default = true)]
+        public bool Notifications { get; set; }
+
+        [Option('r', "remote", HelpText = "Remote server to push updates")]
+        public string Remote { get; set; }
+    }
+}
---
+++ src/ironmunge/IronmungeWorker.cs
@@ -0,0 +1,63 @@
+﻿using Ironmunge.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ironmunge
+{
+    public class IronmungeWorker : BackgroundService
+    {
+        const string IronmungeMutexName = nameof(ironmunge);
+
+        private readonly IServiceProvider _services;
+        private readonly ILogger<IronmungeWorker> _logger;
+        private readonly IOptions<IronmungeOptions> _options;
+
+        //private readonly 
+
+        public IronmungeWorker(IServiceProvider services, ILogger<IronmungeWorker> logger, IOptions<IronmungeOptions> options)
+        {
+            _services = services;
+            _logger = logger;
+            _options = options;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var mutex = new Mutex(true, IronmungeMutexName, out bool createdMutex);
+            if (!createdMutex)
+            {
+                _logger.LogError("ironmunge is already running. Please close any running instances and try again.");
+                return;
+            }
+
+            var o = _options.Value;
+
+            //using (var scope = _services.CreateScope())
+            {
+                var selectedGame = _services.GetServices<IGame>().SingleOrDefault(g => g.Name.Equals(o.Game, StringComparison.InvariantCultureIgnoreCase));
+                if (selectedGame == null)
+                {
+                    _logger.LogError("Game {game} was not found in the list of supported games.", o.Game);
+                    return;
+                }
+
+                var sml = _services.GetRequiredService<ILogger<SaveMonitoring>>();
+                using var cm = new SaveMonitoring(sml, o.GitLocation ?? Ironmunge.Common.Options.DefaultGitPath, o.SaveGameLocation,
+                                                  selectedGame, o.SaveHistoryLocation ?? o.SaveGameLocation,
+                                                  o.Remote)
+                {
+                    PlayNotifications = o.Notifications
+                };
+
+                _logger.LogInformation("ironmunge is now running.");
+                await Task.Delay(-1, stoppingToken);
+            }
+        }
+    }
+}
